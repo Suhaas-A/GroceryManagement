0c091bdc75af873240e70f000b1d122fea5c9f71
Suhaas-A <155752982+Suhaas-A@users.noreply.github.com>
2024-04-30 23:37:09 +0530

    Update settings.py

--- Backend/settings.py
+++ Backend/settings.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-app = Flask(__name__, static_folder='../Frontend/dist/static', template_folder='../Frontend/dist')
+app = Flask(__name__)
 CORS(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
